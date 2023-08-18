1f43947327859602a938cf57cf00552c6dc40b07
DanPark13 <dpark0703@gmail.com>
2023-08-18 12:29:15 -0400

    Complete Foundations 4 Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 || num2) < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    } else {
+        let result = 0
+        if (num1 > num2){
+            temp = num1
+            num1 = num2
+            num2 = temp
+        }
+        for (let i = num1; i <= num2; i++){
+            result += i;
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // If divisible by 100 and 400, then must be divisible by 4
+    // If divisible by 400, then must be divisible by 100
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 };
 
 // Do not edit below this line
